8e43e24898a0feec23d5c88fac434201ea9cbc43
JoshuaDouce <Joshuadouce@gmail.com>
2019-04-11 21:25:28 +0100

    Add project files.

---
+++ ToDoApi/Controllers/RootController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApi.Controllers
+{
+    //Controller base is a stripped down controller it excludes features you only need in a web app
+    [Route("/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot() {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null)
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ ToDoApi/Controllers/ToDoItemController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    //This attribute indicates that the controller responds to web API requests.
+    [ApiController]
+    public class ToDoItemController : ControllerBase
+    {
+        private readonly ToDoItemContext _toDoItemContext;
+
+        public ToDoItemController(ToDoItemContext toDoItemContext)
+        {
+            _toDoItemContext = toDoItemContext;
+
+            if (_toDoItemContext.ToDoItems.Count() == 0)
+            {
+                _toDoItemContext.ToDoItems.Add(new ToDoItem { Name = "Item 1"});
+                _toDoItemContext.SaveChanges();
+            }
+        }
+
+        // GET: api/ToDO
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetToDoItems()
+        {
+            return await _toDoItemContext.ToDoItems.ToListAsync();
+        }
+
+        // GET api/ToDo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoItem>> GetToDoItem(long id)
+        {
+            var todoItem = await _toDoItemContext.ToDoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
