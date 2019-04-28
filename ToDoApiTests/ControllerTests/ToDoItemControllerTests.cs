bf5a03bfcb919e06dac44e3cece3edc8a93c1a7d
JoshuaDouce <Joshuadouce@gmail.com>
2019-04-28 11:58:03 +0100

    Additional to do item controller tests added

--- ToDoApiTests/ControllerTests/ToDoItemControllerTests.cs
+++ ToDoApiTests/ControllerTests/ToDoItemControllerTests.cs
@@ -13,17 +13,18 @@ namespace ToDoApiTests.ControllerTests
     {
         ToDoItemController _toDoItemController;
         private Mock<IToDoItemService> _toDoItemService;
+        IOptions<PagingOptions> pagingOptions;
 
         public ToDoItemControllerTests()
         {
             _toDoItemService = new Mock<IToDoItemService>();
 
-            var options = Options.Create(new PagingOptions() {
+            pagingOptions = Options.Create(new PagingOptions() {
                 Offset = 0,
-                Limit = 5
+                Limit = 2
             });
-
-            _toDoItemController = new ToDoItemController(_toDoItemService.Object, options);
+ 
+            _toDoItemController = new ToDoItemController(_toDoItemService.Object, pagingOptions);
         }
 
         [Fact]
@@ -57,6 +58,64 @@ namespace ToDoApiTests.ControllerTests
             Assert.IsType<ToDoItem>(result.Value);
             Assert.Equal("Item Three", result.Value.Name);
             Assert.True(result.Value.IsComplete);
+        }
+
+        [Fact]
+        public async void Delete_InvalidId_ReturnsNotFound() {
+            //Arrange 
+            _toDoItemService.Setup(r => r.DeleteToDoItemAsync(1))
+                .Returns(Task.FromResult<ToDoItemResponse>(null));
+
+            //Act
+            var result = await _toDoItemController.Delete(1);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async void Delete_ValidId_ReturnsNoContent()
+        {
+            //Arrange 
+            _toDoItemService.Setup(r => r.DeleteToDoItemAsync(1))
+                .Returns(Task.FromResult(new ToDoItemResponse()));
+
+            //Act
+            var result = await _toDoItemController.Delete(1);
+
+            // Assert
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public async void Put_InvalidId_ReturnsBadRequest() {
+            //Arrange 
+            var item = new ToDoItem();
+
+            _toDoItemService.Setup(r => r.PutToDoItemAsync(1, item))
+                .Returns(Task.FromResult<ToDoItemResponse>(null));
+
+            //Act
+            var result = await _toDoItemController.Put(1, item);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public async void Put_ValidId_ReturnsNoContent()
+        {
+            //Arrange 
+            var item = new ToDoItem();
+
+            _toDoItemService.Setup(r => r.PutToDoItemAsync(1, item))
+                .Returns(Task.FromResult(new ToDoItemResponse()));
+
+            //Act
+            var result = await _toDoItemController.Put(1, item);
+
+            // Assert
+            Assert.IsType<NoContentResult>(result);
         }
     }
 }
